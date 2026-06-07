53337412c57760b90434f6a25b1dbcf36f35d673
Chandan Singh <chandan7870827821@gmail.com>
2026-06-07 16:29:43 +0530

    Update CORS origin to new URL

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://mahekdhup.blogspot.com",
+  origin: "https://computerchachaportal.blogspot.com",
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"]
 }));
